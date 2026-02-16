4e31d6a4e26e3369013a1bac81df7e17ed73526b
GUC <gustac@gmail.com>
2026-02-16 16:01:25 -0300

    flask thread parameter added

--- orchestra_any.py
+++ orchestra_any.py
@@ -172,5 +172,5 @@ def index():
 if __name__ == '__main__':
     # For local development, host='0.0.0.0' makes it accessible from other devices on your local network
     # and not just from localhost.
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True, threaded=True)
 
